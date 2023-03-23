618bc565e9c01f394d9582c4ff66d077319ac7e3
tekuto <Hurmfulcookie@gmail.com>
2023-03-23 03:52:42 +0300

    no updater

--- main.py
+++ main.py
@@ -1,59 +1,71 @@
-import logging
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
-import db_utils
+import telebot
+import random
+import sqlite3
+from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 
-# Enable logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-logger = logging.getLogger(__name__)
+# Создаем соединение с базой данных
+conn = sqlite3.connect('questions_actions.db')
 
-# Define the states of the conversation
-SELECT = range(1)
+# Создаем курсор
+cursor = conn.cursor()
 
-# Define the buttons for the main menu
-menu_buttons = [
-    [InlineKeyboardButton("Правда", callback_data='truth')],
-    [InlineKeyboardButton("Действие", callback_data='action')],
-]
+# Создаем таблицу, если ее не
+cursor.execute("""CREATE TABLE IF NOT EXISTS questions_actions (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    text TEXT NOT NULL,
+                    type TEXT NOT NULL
+                )""")
 
-# Define the buttons for the truth and action menus
-truth_buttons = [
-    [InlineKeyboardButton("Так", callback_data='true')],
-    [InlineKeyboardButton("Нет", callback_data='false')],
-    [InlineKeyboardButton("Не знаю", callback_data='unknown')],
-]
+# Добавляем вопросы и действия в таблицу
+cursor.execute("INSERT INTO questions_actions (text, type) VALUES (?, ?)", ("Какое ваше любимое блюдо?", "truth"))
+cursor.execute("INSERT INTO questions_actions (text, type) VALUES (?, ?)", ("Опишите свой худший опыт в ресторане.", "truth"))
+cursor.execute("INSERT INTO questions_actions (text, type) VALUES (?, ?)", ("Какая ваша любимая книга?", "truth"))
+cursor.execute("INSERT INTO questions_actions (text, type) VALUES (?, ?)", ("Опишите свой худший опыт на работе.", "truth"))
+cursor.execute("INSERT INTO questions_actions (text, type) VALUES (?, ?)", ("Попросите у прохожего 10 рублей.", "action"))
+cursor.execute("INSERT INTO questions_actions (text, type) VALUES (?, ?)", ("Съешьте половину лимона.", "action"))
+cursor.execute("INSERT INTO questions_actions (text, type) VALUES (?, ?)", ("Поцелуйте в щеку случайного человека на улице.", "action"))
+cursor.execute("INSERT INTO questions_actions (text, type) VALUES (?, ?)", ("Сходите в магазин и купите конфеты за свой счет.", "action"))
+cursor.execute("INSERT INTO questions_actions (text, type) VALUES (?, ?)", ("Забегите на месте 1 минуту.", "action"))
 
-action_buttons = [
-    [InlineKeyboardButton("Сделано", callback_data='done')],
-    [InlineKeyboardButton("Не сделано", callback_data='not_done')],
-    [InlineKeyboardButton("Не понятно", callback_data='not_understand')],
-]
+# Сохраняем изменения в базе данных
+conn.commit()
 
-# Define the callback functions for the buttons
-def menu_callback(update, context):
-    query = update.callback_query
-    query.answer()
-    context.bot.send_message(chat_id=query.message.chat_id, text='Выберите категорию:', reply_markup=InlineKeyboardMarkup(menu_buttons))
-    return SELECT
+# Закрываем соединение
+conn.close()
 
-def truth_callback(update, context):
-    query = update.callback_query
-    query.answer()
-    context.user_data['category'] = 'truth'
-    data = db_utils.get_random_data('questions')
-    if data:
-        context.user_data['question_id'] = data[0]
-        context.bot.send_message(chat_id=query.message.chat_id, text=data[1], reply_markup=InlineKeyboardMarkup(truth_buttons))
-    else:
-        context.bot.send_message(chat_id=query.message.chat_id, text='Извините, в настоящее время в базе данных нет вопросов в этой категории')
 
-def action_callback(update, context):
-    query = update.callback_query
-    query.answer()
-    context.user_data['category'] = 'action'
-    data = db_utils.get_random_data('actions')
-    if data:
-        context.user_data['question_id'] = data[0]
-        context.bot.send_message(chat_id=query.message.chat_id, text=data[1], reply_markup=InlineKeyboardMarkup(action_buttons))
-    else:
-        context.bot.send_message(chat_id=query.message.chat_id, text='Извините, в
+bot = telebot.TeleBot('5770545685:AAHCbhMgND_CIPy2Ru0rfFrevYWXmIFOFDU')
+
+
+def get_random_question():
+    cursor.execute('SELECT truth FROM questions_actions ORDER BY RANDOM() LIMIT 1')
+    question = cursor.fetchone()
+    return question[0]
+
+def get_random_action():
+    cursor.execute('SELECT action FROM questions_actions ORDER BY RANDOM() LIMIT 1')
+    action = cursor.fetchone()
+    return action[0]
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "Привет! Я бот для игры в \"Правда или Действие\". Напиши /game, чтобы начать игру.")
+
+@bot.message_handler(commands=['game'])
+def game(message):
+    markup = ReplyKeyboardMarkup(resize_keyboard=True)
+    truth_button = KeyboardButton('Правда')
+    action_button = KeyboardButton('Действие')
+    markup.add(truth_button, action_button)
+    bot.send_message(message.chat.id, "Выбери категорию:", reply_markup=markup)
+
+@bot.message_handler(func=lambda message: True)
+def process_game(message):
+    if message.text == 'Правда':
+        question = get_random_question()
+        bot.send_message(message.chat.id, "Вопрос: " + question)
+    elif message.text == 'Действие':
+        action = get_random_action()
+        bot.send_message(message.chat.id, "Действие: " + action)
+
+bot.polling()
